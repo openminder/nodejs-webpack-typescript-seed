dbd97c5d7946dd2ff6547f7d8cfc5b9ca2f89477
Unknown <martin.koehn@googlemail.com>
2017-06-08 23:20:03 +0200

    Add logger middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import * as Koa from 'koa';
+import * as logger from 'koa-logger';
 import Config from './config';
 import SampleService from './services/sample.service';
 
 const sampleService = new SampleService();
 const app = new Koa();
 const conf = new Config();
+
+app.use(logger());
 
 app.use( (ctx) => {
   ctx.body = 'Hello World';
