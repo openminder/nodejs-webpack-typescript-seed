f8be55d25b533a5a0ebd82274dd5c847462a8872
Unknown <martin.koehn@googlemail.com>
2017-06-08 22:31:42 +0200

    add koa

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
+import * as Koa from 'koa';
 import SampleService from './services/sample.service';
 
 const sampleService = new SampleService();
+const app = new Koa();
 
+app.use( (ctx) => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(3000);
 console.log('App started');
